ae3d97248f3ce6ee521c8cdb2d816fb226dbefd3
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-04-28 13:50:14 +0900

    課題２の提出

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -1,12 +1,16 @@
 <?php
 $array = explode(',', $_GET['array']);
-
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-
+    for ($j = 0; $j < count($array)-1; $j++) {
+        if ($array[$j] > $array[$j+1]) {
+            $tmp = $array[$j];
+            $array[$j] = $array[$j+1];
+            $array[$j+1] = $tmp;
+        }
+    }
 }
 // 修正はここまで
-
 echo "<pre>";
 print_r($array);
 echo "</pre>";
