7dca4d07e80437961d509835b64d1bc26e8ffc54
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-05-18 20:04:26 +0900

    意味なし

--- html/php-challenge/join/index.php
+++ html/php-challenge/join/index.php
@@ -1,9 +1,57 @@
 <?php
-if ($_POST['name'] === '') {
-	print('名前が入力されていません');
+require('../dbconnect.php');
+
+session_start();
+
+if (!empty($_POST)) {
+	// エラー項目の確認
+	if ($_POST['name'] == '') {
+		$error['name'] = 'blank';
+	}
+	if ($_POST['email'] == '') {
+		$error['email'] = 'blank';
+	}
+	if (strlen($_POST['password']) < 4) {
+		$error['password'] = 'length';
+	}
+	if ($_POST['password'] == '') {
+		$error['password'] = 'blank';
+	}
+	$fileName = $_FILES['image']['name'];
+	if (!empty($fileName)) {
+		$ext = substr($fileName, -3);
+		if ($ext != 'jpg' && $ext != 'gif') {
+			$error['image'] = 'type';
+		}
+	}
+
+	// 重複アカウントのチェック
+	if (empty($error)) {
+		$member = $db->prepare('SELECT COUNT(*) AS cnt FROM members WHERE email=?');
+		$member->execute(array($_POST['email']));
+		$record = $member->fetch();
+		if ($record['cnt'] > 0) {
+			$error['email'] = 'duplicate';
+		}
+	}
+
+	if (empty($error)) {
+		// 画像をアップロードする
+		$image = date('YmdHis') . $_FILES['image']['name'];
+		move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' . $image);
+
+		$_SESSION['join'] = $_POST;
+		$_SESSION['join']['image'] = $image;
+		header('Location: check.php');
+		exit();
+	}
 }
 
-
+// 書き直し
+if ($_REQUEST['action'] == 'rewrite') {
+	$_POST = $_SESSION['join'];
+	$error['rewite'] = true;
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
